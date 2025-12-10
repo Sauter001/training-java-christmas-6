174b64e61ef76a8645b9fc70d4044c9729a92df0
sauter001 <lsj457@naver.com>
2025-12-10 21:39:17 +0900

    refactor: remove unused imports

--- src/main/java/christmas/domain/gift/Gift.java
+++ src/main/java/christmas/domain/gift/Gift.java
@@ -1,9 +1,6 @@
 package christmas.domain.gift;
 
-import christmas.domain.Dish;
-import christmas.domain.dto.DishNameDto;
-
-public record Gift(String  name, int quantity, int price) {
+public record Gift(String name, int quantity, int price) {
     public static Gift from(GiftProduct giftProduct) {
         return new Gift(giftProduct.dishName(), 1, giftProduct.giftPrice());
     }
